c450cfad2a37a32ea68f660642128ae4a2b1d654
Pablo Alan <pabloxt14@gmail.com>
2024-05-24 19:41:11 -0300

    feat: dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 export default function Home() {
   return (
-    <main className="min-h-screen w-full  bg-zinc-900 p-4 text-zinc-100 ">
-      <h1 className="flex items-center gap-2 text-3xl font-extrabold before:h-8 before:w-1 before:rounded-full before:bg-violet-500 sm:text-4xl lg:text-5xl">
-        Hello World
-      </h1>
+    <main className="min-h-screen w-full bg-zinc-100 p-12 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100 ">
+      <div className="mx-auto flex max-w-3xl flex-col items-center gap-4 text-center">
+        <h1 className="flex items-center gap-2 text-3xl font-extrabold sm:text-4xl lg:text-5xl">
+          Rapidly build modern websites without ever leaving your HTML.
+        </h1>
 
-      <p className="my-4">This is the home page</p>
+        <p className="my-4 max-w-xl">
+          A utility-first CSS framework packed with classes like flex, pt-4,
+          text-center and rotate-90 that can be composed to build any design,
+          directly in your markup.
+        </p>
 
-      <button
-        disabled
-        className="rounded-md bg-violet-500 px-4 py-2 font-medium text-zinc-100 transition-colors enabled:hover:bg-violet-600 disabled:cursor-not-allowed disabled:opacity-60"
-      >
-        See more
-      </button>
+        <button className="rounded-md bg-zinc-900 px-4 py-2 font-medium text-zinc-100 transition-colors hover:bg-zinc-700 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-violet-500 dark:text-zinc-100 dark:enabled:hover:bg-violet-600">
+          Get started
+        </button>
+      </div>
     </main>
   )
 }
