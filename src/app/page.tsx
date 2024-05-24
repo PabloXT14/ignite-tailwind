f787baf754423e1635aa131b627690abf2c9451e
Pablo Alan <pabloxt14@gmail.com>
2024-05-24 19:13:04 -0300

    feat: selectors and states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 export default function Home() {
   return (
-    <main>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" className="bg-zinc-700" />
-      </div>
+    <main className="min-h-screen w-full  bg-zinc-900 p-4 text-zinc-100 ">
+      <h1 className="flex items-center gap-2 text-4xl font-extrabold before:h-8 before:w-1 before:rounded-full before:bg-violet-500">
+        Hello World
+      </h1>
 
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" className="bg-zinc-700" />
-      </div>
+      <p className="my-4">This is the home page</p>
+
+      <button
+        disabled
+        className="rounded-md bg-violet-500 px-4 py-2 font-medium text-zinc-100 transition-colors enabled:hover:bg-violet-600 disabled:cursor-not-allowed disabled:opacity-60"
+      >
+        See more
+      </button>
     </main>
   )
 }
