5950961830fd555c098627090fe44254b2d00150
Pablo Alan <pabloxt14@gmail.com>
2024-05-24 23:29:44 -0300

    feat: create navigation menu

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,12 +1,13 @@
 import { LuSearch } from 'react-icons/lu'
 import { Logo } from './logo'
+import { MainNavigation } from './main-navigation'
 
 export const Sidebar = () => {
   return (
     <aside className="space-y-6 overflow-y-auto border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <LuSearch className="size-5 text-zinc-500" />
         <input
           type="text"
@@ -14,6 +15,8 @@ export const Sidebar = () => {
           className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
         />
       </div>
+
+      <MainNavigation />
     </aside>
   )
 }
--- src/components/sidebar/logo.tsx
+++ src/components/sidebar/logo.tsx
@@ -2,7 +2,7 @@ import { LogoIcon } from '@/assets/logo-icon'
 
 export const Logo = () => {
   return (
-    <strong className="flex items-center gap-2 text-xl font-semibold text-zinc-900">
+    <strong className="mx-1 flex items-center gap-2 text-xl font-semibold text-zinc-900">
       <LogoIcon />
       <span>Untitled UI</span>
     </strong>
