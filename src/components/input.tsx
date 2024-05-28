63b4ac233c84ff4c187630a38105fcbdfebaab5f
Pablo Alan <pabloxt14@gmail.com>
2024-05-28 20:23:02 -0300

    feat: form fields

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -12,7 +12,7 @@ const InputControl = (props: InputControlProps) => {
   return (
     <input
       type="text"
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      className="w-full flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
       {...props}
     />
   )
