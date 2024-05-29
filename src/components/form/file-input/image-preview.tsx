748732932c85adfbb31b05ea11eeed33148326d1
Pablo Alan <pabloxt14@gmail.com>
2024-05-28 23:05:08 -0300

    feat: create component file input

---
+++ src/components/form/file-input/image-preview.tsx
@@ -0,0 +1,9 @@
+import { LuUser } from 'react-icons/lu'
+
+export const ImagePreview = () => {
+  return (
+    <div className="flex size-16 items-center justify-center rounded-full bg-violet-50">
+      <LuUser className="size-8 text-violet-500" />
+    </div>
+  )
+}
