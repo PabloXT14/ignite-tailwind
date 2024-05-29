810b0fb0ef6c2c03928e51001c2cb9f697878ebf
Pablo Alan <pabloxt14@gmail.com>
2024-05-28 23:39:46 -0300

    feat: create context and image preview

--- src/components/form/file-input/image-preview.tsx
+++ src/components/form/file-input/image-preview.tsx
@@ -1,9 +1,32 @@
+'use client'
+
 import { LuUser } from 'react-icons/lu'
+import { useFileInput } from './root'
+import { useMemo } from 'react'
 
 export const ImagePreview = () => {
+  const { files } = useFileInput()
+
+  const previewURL = useMemo(() => {
+    if (files.length === 0) return null
+
+    return URL.createObjectURL(files[0])
+  }, [files])
+
+  if (previewURL === null) {
+    return (
+      <div className="flex size-16 items-center justify-center rounded-full bg-violet-50">
+        <LuUser className="size-8 text-violet-500" />
+      </div>
+    )
+  }
+
   return (
-    <div className="flex size-16 items-center justify-center rounded-full bg-violet-50">
-      <LuUser className="size-8 text-violet-500" />
-    </div>
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={previewURL}
+      alt=""
+      className="size-16 rounded-full object-cover"
+    />
   )
 }
