8596427eac3649a66665f2cbdaa1a059245e3e26
Pablo Alan <pabloxt14@gmail.com>
2024-05-30 20:42:47 -0300

    feat: animating select's opening

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,24 @@ const config: Config = {
           25: '#fcfaff',
         },
       },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        // Não utilizaremos pois o RadixUI deleta o Content do Select quando o Select é fechado
+        // slideUpAndFade: {
+        //   from: { opacity: '1', transform: 'translateY(0)' },
+        //   to: { opacity: '0', transform: 'translateY(-2px)' },
+        // },
+      },
+
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        // slideUpAndFade: 'slideUpAndFade 1s linear',
+      },
     },
   },
   plugins: [],
