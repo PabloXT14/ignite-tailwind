c4d6f6b4413002afce064dc8bbc236088a43b3c6
Pablo Alan <pabloxt14@gmail.com>
2024-06-03 21:08:12 -0300

    feat: create dark sidebar dark theme

--- src/components/sidebar/logo.tsx
+++ src/components/sidebar/logo.tsx
@@ -2,7 +2,7 @@ import { LogoIcon } from '@/assets/logo-icon'
 
 export const Logo = () => {
   return (
-    <strong className="mx-1 flex items-center gap-2 text-xl font-semibold text-zinc-900">
+    <strong className="mx-1 flex items-center gap-2 text-xl font-semibold text-zinc-900 dark:text-zinc-100">
       <LogoIcon />
       <span>Untitled UI</span>
     </strong>
--- src/components/sidebar/profile.tsx
+++ src/components/sidebar/profile.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { LuLogOut } from 'react-icons/lu'
 import { Button } from '../button'
 
@@ -10,10 +11,10 @@ export const Profile = () => {
         className="size-10 rounded-full"
       />
       <div className="flex flex-col truncate">
-        <span className="truncate text-sm font-semibold text-zinc-700">
+        <span className="truncate text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Pablo Alan
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           pabloxt14@gmail.com
         </span>
       </div>
